3217672d0d7cd3c7102e903e76a7375def897e10
William Boone <138145899+boonewh@users.noreply.github.com>
2024-03-20 22:27:38 -0500

    Added contact page

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from forms import ContactForm
+from flask_mail import Mail, Message
+from config import EMAIL_PASSWORD, EMAIL_ADDRESS
 
 app = Flask(__name__)
+app.secret_key = 'pathsix033170'
+
+mail = Mail()
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = EMAIL_ADDRESS
+app.config["MAIL_PASSWORD"] = EMAIL_PASSWORD
+mail.init_app(app)
 
 @app.route('/')
 def index():
@@ -10,9 +22,25 @@ def index():
 def blog():
     return render_template('blog.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+  form = ContactForm()
+  if request.method == 'POST':
+    if form.validate() == False:
+      flash('All fields are required.')
+      return render_template('contact.html', form=form)
+    else:
+      msg = Message(form.subject.data, sender=EMAIL_ADDRESS, recipients=[EMAIL_ADDRESS])
+      msg.body = """ 
+        This message was sent from the portfolio contact form.
+
+        From: %s <%s> 
+        %s 
+        """ % (form.name.data, form.email.data, form.message.data)
+      mail.send(msg)
+      return render_template('contact.html', success=True)
+  elif request.method == 'GET':
+    return render_template('contact.html', form=form)
 
 @app.route('/coffee')
 def coffee():
