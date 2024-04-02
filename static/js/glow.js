e6785ec22eb9788a4199b6a86421f5227091e1eb
William Boone <138145899+boonewh@users.noreply.github.com>
2024-04-01 22:24:45 -0500

    Updated Project Descriptions

--- static/js/glow.js
+++ static/js/glow.js
@@ -1,16 +1,27 @@
 window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY || window.pageYOffset;
-    var maxHeight = document.body.scrollHeight - window.innerHeight;
-    var scrollRatio = scrollPosition / maxHeight;
-  
-    // Adjust the glow intensity based on the scroll position
-    var glowIntensity = scrollRatio * 10; // Adjust the multiplier as needed
-  
-    // Update the glow color from white to yellow based on the scroll
-    var red = 255; // Red stays 255 for yellow
-    var green = 255 * scrollRatio; // Green goes from 255 to 0
-    var blue = 255 * (1 - scrollRatio); // Blue goes from 255 to 0
-  
-    // Apply the glow effect to the ::before element
-    document.querySelector('.resume::before').style.boxShadow = `0 0 ${glowIntensity}px rgba(${red}, ${green}, ${blue}, 1)`;
-  });
+  var scrollPosition = window.scrollY || window.pageYOffset;
+  var maxHeight = document.body.scrollHeight - window.innerHeight;
+  var scrollRatio = scrollPosition / maxHeight;
+
+  // Adjust the glow intensity based on the scroll position
+  var glowIntensity = scrollRatio * 10; // Adjust the multiplier as needed
+
+  // Update the glow color from white to yellow based on the scroll
+  var red = 255; // Red stays 255 for yellow
+  var green = 255 * scrollRatio; // Green goes from 255 to 0
+  var blue = 255 * (1 - scrollRatio); // Blue goes from 255 to 0
+
+  // Construct the CSS rule
+  var css = `.resume::before { box-shadow: 0 0 ${glowIntensity}px rgba(${red}, ${green}, ${blue}, 1); }`;
+
+  // Check if our style tag exists, if not, create it
+  var styleTag = document.getElementById('dynamicPseudoStyles');
+  if (!styleTag) {
+      styleTag = document.createElement('style');
+      styleTag.id = 'dynamicPseudoStyles';
+      document.head.appendChild(styleTag);
+  }
+
+  // Update the style tag's content
+  styleTag.innerHTML = css;
+});
